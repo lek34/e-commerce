de306f945a1c0dbbafa2500bda352cca8be92db1
Winar Joko Alexander <winaralex417@gmail.com>
2024-02-15 07:49:41 +0700

    Initial commit

---
+++ backend/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue"
+import Login from "../views/Login.vue"
+import RequestPassword from "../views/RequestPassword.vue"
+import ResetPassword from "../views/ResetPassword.vue"
+
+const routes = [
+    {
+        path: '/dasboard',
+        name: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/request-password',
+        name: 'request-password',
+        component: RequestPassword
+    },
+    {
+        path: '/reset-password/token',
+        name: 'reset-password',
+        component: ResetPassword
+    },
+];
+
+
+const router = createRouter({
+    history: createWebHistory(), //domain.com#/users
+    routes
+})
+
+export default router
---
+++ backend/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Dashboard</h1>
+</template>
+
+<script>
+
+export default {
+    name:"Login"
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
